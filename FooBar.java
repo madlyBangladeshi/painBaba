bd9da35d7651623ae25446b0cfa0b55341b3eade
madlyBangladeshi <33605692+madlyBangladeshi@users.noreply.github.com>
2017-11-13 01:23:16 +0600

    test

---
+++ FooBar.java
@@ -0,0 +1,69 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package foobar;
+
+import java.math.BigInteger;
+
+/**
+ *
+ * @author bisho
+ */
+public class FooBar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*Problem 1
+         String s="wrw blf hvv ozhg mrtsg'h vkrhlwv?";
+         s = "Yvzs! I xzm'g yvorvev Lzmxv olhg srh qly zg gsv xlolmb!!";
+         String t="zyxwvutsrqponmlkjihgfedcba";
+         String u="";
+         for(int i=0;i<s.length();i++){
+         if(s.charAt(i) >='a' && s.charAt(i) <='z'){
+         //s.charAt(i)='a';
+         //System.out.print(t.charAt(s.charAt(i)-'a'));
+         u+=t.charAt(s.charAt(i)-'a');
+         }else{
+         u+=s.charAt(i);
+         }
+            
+         }
+         return u;
+         * */
+        /*
+         Prob2 p = new Prob2();
+         System.out.print(p.calc(0, 15) + "\t");
+         /*for (int i = 0; i < 64; i++) {
+         for (int j = 0; j < 64; j++) {
+         System.out.print(p.calc(i, j) + "\t");
+         }
+         System.out.println("");
+         }*/
+
+        /********************Problem 3****************/
+        /*
+        int x = 2;
+        int y = 1;
+   
+        long nx = x, ny = y;
+        long sum=0;
+        nx += y - 1;
+        ny = 1;
+        if (nx % 2 == 0) {
+            sum=nx * (nx + 1) / 2 - (y - 1);
+        } else {
+            sum=nx * (nx + 1) / 2 - (nx - y);
+        }
+        return Long.toString(sum);
+        /********************  End  *****************/
+
+        
+
+
+
+    }
+}
